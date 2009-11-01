43a90e3f17dca240d38e232cf5a3d85d14c51f9d
Moritz Lenz <moritz@faui2k3.org>
2009-11-01 18:07:49 +0100

    simplify action methods
    Somehow it shows that they were written in Rakudo's early days, and also that
    I didn't have much Perl 6 experience back then.

--- lib/JSON/Tiny/Actions.pm
+++ lib/JSON/Tiny/Actions.pm
@@ -4,40 +4,19 @@ method TOP($/, $what) {
     make $/{$what}.ast;
 };
 method object($/) {
-    make $<pairlist>.ast ?? hash ( $<pairlist>.ast ) !! {};
+    make %($<pairlist>.ast)
 }
 
 method pairlist($/) {
-    if $<pair> {
-        my %r;
-        for $<pair>.map(*.ast) -> $m {
-            %r{$m<key>} = $m<value>;
-        }
-        make %r;
-    }
-    else {
-        make undef;
-    }
+    make = $<pair>».ast;
 }
 
 method pair($/) {
-    make {
-        key   => $<string>.ast,
-        value => $<value>.ast,
-    };
+    make ( $<string>.ast => $<value>.ast );
 }
 
 method array($/) {
-    if $<value> {
-        my @r = ();
-        for $<value>>>.ast {
-            when Hash { @r.push: \$_ }
-            default   { @r.push:  $_ }
-        }
-        make @r
-    } else {
-        make [];
-    }
+    make $<value>».ast;
 }
 
 method value($/, $what) {
