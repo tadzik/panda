24461fe294b02aff05c2463a45d6fe1b8771d14e
Tadeusz Sośnierz <tadzikes@gmail.com>
2010-08-12 21:17:49 +0000

    Initial commit

---
+++ File/Tools.pm
@@ -0,0 +1,15 @@
+module File::Tools:<github:tadzik>;
+
+=begin pod
+
+This is a distribution for file-related utilities, including:
+
+=item File::Copy
+=item File::Find
+=item File::Mkdir
+
+Please refer to their documentation for more information.
+
+=end pod
+
+# vim: ft=perl6
