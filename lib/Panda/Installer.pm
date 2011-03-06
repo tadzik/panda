cf33281b074062c8d66bd202f28edf351206ca5e
Tadeusz Sośnierz <tadzikes@gmail.com>
2011-03-06 13:26:25 +0100

    Panda::Installer is now pureperl, add some new tests for that

--- lib/Panda/Installer.pm
+++ lib/Panda/Installer.pm
@@ -1,5 +1,7 @@
 use Pies;
 use Panda::Common;
+use File::Find;
+use File::Mkdir;
 
 class Panda::Installer does Pies::Installer {
     has $!srcdir;
@@ -7,11 +9,15 @@ class Panda::Installer does Pies::Installer {
 
     method install(Pies::Project $p) {
         indir "$!srcdir/{dirname $p.name}", {
-            unless 'Makefile'.IO ~~ :f {
-                run 'ufo' and die 'ufo failed';
+            for find(dir => 'blib', type => 'file').list -> $i {
+                # .substr(5) to skip 'blib/'
+                mkdir "$!destdir/{$i.dir.substr(5)}", :p;
+                $i.IO.copy("$!destdir/{$i.Str.substr(5)}");
             }
-            my $infix = $!destdir ?? "PREFIX=$!destdir" !! '';
-            run "make $infix install" and die "Installing failed";
+            for find(dir => 'bin', type => 'file').list -> $bin {
+                mkdir "$!destdir/{$bin.dir}", :p;
+                $bin.IO.copy("$!destdir/$bin");
+            }
         };
     }
 }
--- t/panda/installer.t
+++ t/panda/installer.t
@@ -1,18 +1,20 @@
 use Test;
 use Panda::Installer;
 
-plan 2;
+plan 4;
 
 my $srcdir  = 'testmodules';
 my $destdir = "{cwd}/removeme";
 
 my $b = Panda::Installer.new(srcdir => $srcdir, destdir => $destdir);
 
-my $p = Pies::Project.new(name => 'dummymodule');
+my $p = Pies::Project.new(name => 'compiledmodule');
 
 lives_ok { $b.install($p) };
 
-ok "$destdir/lib/foo.pm".IO ~~ :f, 'module installed';
+ok "$destdir/lib/foo.pm".IO  ~~ :f, 'module installed';
+ok "$destdir/lib/foo.pir".IO ~~ :f, 'pir installed';
+ok "$destdir/bin/bar".IO     ~~ :f, 'bin installed';
 
 run "rm -rf $destdir";
 
