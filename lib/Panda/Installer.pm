288abacfbc603d0c0907df8568c22be70d7f74fc
Tadeusz Sośnierz <tadzikes@gmail.com>
2011-03-06 13:46:15 +0100

    Panda::Installer bugfix

--- lib/Panda/Installer.pm
+++ lib/Panda/Installer.pm
@@ -9,14 +9,18 @@ class Panda::Installer does Pies::Installer {
 
     method install(Pies::Project $p) {
         indir "$!srcdir/{dirname $p.name}", {
-            for find(dir => 'blib', type => 'file').list -> $i {
-                # .substr(5) to skip 'blib/'
-                mkdir "$!destdir/{$i.dir.substr(5)}", :p;
-                $i.IO.copy("$!destdir/{$i.Str.substr(5)}");
+            if 'blib'.IO ~~ :d {
+                for find(dir => 'blib', type => 'file').list -> $i {
+                    # .substr(5) to skip 'blib/'
+                    mkdir "$!destdir/{$i.dir.substr(5)}", :p;
+                    $i.IO.copy("$!destdir/{$i.Str.substr(5)}");
+                }
             }
-            for find(dir => 'bin', type => 'file').list -> $bin {
-                mkdir "$!destdir/{$bin.dir}", :p;
-                $bin.IO.copy("$!destdir/$bin");
+            if 'bin'.IO ~~ :d {
+                for find(dir => 'bin', type => 'file').list -> $bin {
+                    mkdir "$!destdir/{$bin.dir}", :p;
+                    $bin.IO.copy("$!destdir/$bin");
+                }
             }
         };
     }
