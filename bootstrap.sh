075b257322684decda6dd04cfa9d77ef0c0a5771
Tadeusz Sośnierz <tadzikes@gmail.com>
2011-03-11 00:00:25 +0100

    Adjust boostrap.sh to rakudo-star branch: install ufo, and then install Pies using ufo. It can't install itself, for it always fetches the master branch from git

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 PWD=`pwd`
-PERL6LIB=$PWD/ext:$PWD/lib perl6 bin/panda install Pies
+PERL6LIB=$PWD/ext:$PWD/lib perl6 bin/panda install File::Tools JSON::Tiny Test::Mock ufo
+ufo
+make test
+make install
+rm Makefile
