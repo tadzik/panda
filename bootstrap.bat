03841dd38dc25a077c278521f64b4d59c86b2d63
Tadeusz Sośnierz <tadzikes@gmail.com>
2012-04-22 15:42:19 +0200

    Fix bootstrap (sort of)

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,2 +1,4 @@
 SET PERL6LIB=%CD%\ext;%CD%\lib 
-perl6 bin\panda install .
+perl6 bin\panda install File::Tools JSON::Tiny Test::Mock
+SET PERL6LIB=%CD%\lib
+perl6 bin\panda install panda
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 PWD=`pwd`
-PERL6LIB=$PWD/ext:$PWD/lib perl6 bin/panda install .
+PERL6LIB=$PWD/ext:$PWD/lib perl6 bin/panda install File::Tools JSON::Tiny Test::Mock
+PERL6LIB=lib perl6 bin/panda install panda
