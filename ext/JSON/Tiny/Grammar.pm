269d68c115a00b17d3eddcd715903eec69e216b0
Tadeusz Sośnierz <tadzikes@gmail.com>
2012-05-27 13:06:39 +0200

    Update JSON in ext

--- ext/JSON/Tiny/Grammar.pm
+++ ext/JSON/Tiny/Grammar.pm
@@ -3,10 +3,10 @@ grammar JSON::Tiny::Grammar;
 
 rule TOP        { ^[ <object> | <array> ]$ }
 rule object     { '{' ~ '}' <pairlist>     }
-rule pairlist   { [ <pair>+ % [ \, ]  ]?   }
+rule pairlist   { <pair>* % \,             }
 rule pair       { <string> ':' <value>     }
 rule array      { '[' ~ ']' <arraylist>    }
-rule arraylist  { [ <value>+ % [ \, ] ]?   }
+rule arraylist  { <value>* % [ \, ]        }
 
 proto token value {*};
 token value:sym<number> {
