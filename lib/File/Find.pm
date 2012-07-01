11b01930853911275555dcadf5f8678a86059c28
Moritz Lenz <moritz@faui2k3.org>
2012-07-01 10:18:39 +0200

    update to newest &dir changes

--- lib/File/Find.pm
+++ lib/File/Find.pm
@@ -44,13 +44,9 @@ sub checkrules ($elem, %opts) {
 	return True
 }
 
-sub file-from-path($path) {
-    $path.split('/')[*-1];
-}
-
 sub find (:$dir!, :$name, :$type) is export {
 	my @targets = dir($dir).map: {
-		File::Find::Result.new(dir => $dir, name => file-from-path(.path));
+		File::Find::Result.new(dir => $dir, name => .basename);
 	};
 	my $list = gather while @targets {
 		my $elem = @targets.shift;
@@ -58,7 +54,7 @@ sub find (:$dir!, :$name, :$type) is export {
 		if $elem.IO ~~ :d {
 			for dir($elem) -> $file {
 				@targets.push(
-					File::Find::Result.new(dir => $elem, name => file-from-path($file.path))
+					File::Find::Result.new(dir => $elem, name => $file.basename)
 				);
 			}
 		}
