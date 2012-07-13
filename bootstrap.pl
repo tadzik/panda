04b67556b56edd0c4599fc20c9c7e49a292b0cc1
Moritz Lenz <moritz@faui2k3.org>
2012-07-13 08:29:20 +0200

    make bootstrap.pl more robust
    do not add ext/ to lib once ext/ is installed

--- bootstrap.pl
+++ bootstrap.pl
@@ -1,5 +1,8 @@
 use v6;
 
-%*ENV<PERL6LIB> = "{%*ENV<PERL6LIB> // ''}:{cwd}/ext:{cwd}/lib";
+my $oldenv  = %*ENV<PERL6LIB> // '';
+my $env_sep = $*VM<config><osname> eq 'MSWin32' ?? ';' !! ':';
+%*ENV<PERL6LIB> = join $env_sep, $oldenv, cwd() ~ '/ext', cwd() ~ '/lib';
 shell "perl6 bin/panda install File::Tools JSON::Tiny Test::Mock";
+%*ENV<PERL6LIB> = join $env_sep, $oldenv, cwd() ~ '/lib';
 shell "perl6 bin/panda install .";
