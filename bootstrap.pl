6ca2122d7959ce7c8ba3d2920ee2abd9b0d07614
Geoffrey Broadwell <geoff@broadwell.org>
2012-08-20 16:26:42 -0700

    Make DESTDIR minimally work during bootstrap

--- bootstrap.pl
+++ bootstrap.pl
@@ -18,21 +18,15 @@ given open "$home/.panda/projects.json", :w {
 
 my $oldenv  = %*ENV<PERL6LIB> // '';
 my $env_sep = $*VM<config><osname> eq 'MSWin32' ?? ';' !! ':';
+my $destdir = %*ENV<DESTDIR> || "$home/.perl6";
+   $destdir = "{cwd}/$destdir" unless $destdir ~~ /^ '/' /;
 
-
-if %*ENV<DESTDIR> {
-    %*ENV<PERL6LIB> ~= "{$env_sep}{cwd}/{%*ENV<DESTDIR>}/lib"
-}
-
+%*ENV<PERL6LIB> ~= "{$env_sep}$destdir/lib";
 %*ENV<PERL6LIB> ~= "{$env_sep}{cwd}/ext/File__Tools/lib";
 %*ENV<PERL6LIB> ~= "{$env_sep}{cwd}/ext/JSON__Tiny/lib";
 %*ENV<PERL6LIB> ~= "{$env_sep}{cwd}/ext/Test__Mock/lib";
 %*ENV<PERL6LIB> ~= "{$env_sep}{cwd}/lib";
 
-shell "perl6 bin/panda install File::Tools JSON::Tiny Test::Mock";
-
-%*ENV<PERL6LIB> = join $env_sep, $oldenv, cwd() ~ '/lib';
-
-shell "perl6 bin/panda install {cwd}";
+shell "perl6 bin/panda install File::Tools JSON::Tiny Test::Mock {cwd}";
 
 unlink "$home/.panda/projects.json";
