6c2772259e34f870111a89f92007d20c74c19bff
Tadeusz Sośnierz <tadzikes@gmail.com>
2013-05-03 15:59:15 +0200

    Adjust for IO::Path stringification changes

--- Shell/Command.pm
+++ Shell/Command.pm
@@ -47,7 +47,7 @@ sub mv(*@args) is export {
 sub cp($from as Str, $to as Str, :$r) is export {
     if ($from.IO ~~ :d and $r) {
         mkdir("$to") if $to.IO !~~ :d;
-        for dir($from) -> $item {
+        for dir($from)».basename -> $item {
             mkdir("$to/$item") if "$from/$item".IO ~~ :d;
             cp("$from/$item", "$to/$item", :r);
         }
