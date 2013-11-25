ae6eab59ec9f2f59e81d3642974c74135c2a2321
Tobias Leich <email@froggs.de>
2013-11-25 23:22:43 +0100

    cleanup path to pass use it in substr
    See http://irclog.perlgeek.de/perl6/2013-11-25#i_7914075

--- lib/Panda/Fetcher.pm
+++ lib/Panda/Fetcher.pm
@@ -25,7 +25,8 @@ sub git-fetch($from, $to) {
 
 sub local-fetch($from, $to) {
     for find(dir => $from).list {
-        my $d = $_.directory.substr($from.chars);
+        # We need to cleanup the path, because the returned elems are too.
+        my $d = $_.directory.substr($from.IO.path.cleanup.chars);
         next if $d ~~ /^ '/'? '.git'/; # skip VCS files
         my $where = "$to/$d";
         mkpath $where;
