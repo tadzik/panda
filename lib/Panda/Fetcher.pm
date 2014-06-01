ece2c62b1d5ab0f4c268bdd523c5fec823d4caa8
Tobias Leich <email@froggs.de>
2014-06-01 21:59:28 +0200

    allow to fetch http resources
    We need this to fetch MIRRORED.BY to know what servers are best to query, but we
    also need to fetch the p6dists.json to know what distributions are out there, waiting
    for us.

--- lib/Panda/Fetcher.pm
+++ lib/Panda/Fetcher.pm
@@ -10,6 +10,9 @@ method fetch($from, $to) {
         when *.IO.d {
             local-fetch $from, $to;
         }
+        when /^http '://'/ {
+            http-fetch $from, $to;
+        }
         default {
             fail "Unable to handle source '$from'"
         }
@@ -42,6 +45,49 @@ sub local-fetch($from, $to) {
         $_.copy("$where/{$_.basename}");
     }
     return True;
+}
+
+sub http-fetch($from, $to) {
+    my $s;
+    my $host = $from ~~ /^ [\w+ '://']? $<host>=[ <-[\/]>+ ] / ?? ~$<host> !! die "Could not parse url '$from'";
+    if  %*ENV<http_proxy> {
+        my ($host, $port) = %*ENV<http_proxy>.split('/').[2].split(':');
+        $s = IO::Socket::INET.new(host => $host, port => $port.Int);
+    }
+    else {
+        $s = IO::Socket::INET.new(:$host, :port(80));
+    }
+    $s.send("GET $from HTTP/1.1\nHost: $host\nAccept: */*\nConnection: Close\n\n");
+    my $buf = $s.recv(:bin);
+
+    my $i = 0;
+    my ($header, $new_chunk) = $buf.decode('utf8').split(/\r\n\r\n/, 2);
+    my @chunks = $new_chunk.encode;
+    if $header ~~ /^^ 'Transfer-Encoding:' \N+ chunked / {
+        my ($size, $new_chunk) = @chunks[$i].decode('utf8').split(/\r?\n/, 2);
+        @chunks[$i] = $new_chunk.encode;
+        $size = :16($size);
+        while @chunks[$i].bytes < $size {
+            @chunks[$i] ~= $s.recv(:bin, $size - @chunks[$i].bytes);
+            
+            if $size == @chunks[$i].bytes {
+                $i++;
+                @chunks[$i] ~= $s.recv(10, :bin).decode('utf8').trim-leading;
+                ($size, $new_chunk) = @chunks[$i].split(/\r?\n/, 2);
+                @chunks[$i] = $new_chunk.encode;
+                $size = :16($size);
+            }
+        }
+    }
+
+    given open($to, :w) {
+        .say: @chunks>>.decode('utf8').join('');
+        .close;
+    }
+
+    CATCH {
+        die "Could not download mirrors list: {$_.message}"
+    }
 }
 
 # vim: ft=perl6
