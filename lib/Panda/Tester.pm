a65a03f8aa363a0c76e5f5688545c2088699fcd3
Carlin <cb@viennan.net>
2014-09-24 10:48:27 +1200

    run tests normally if Build.pm is not a Tester
    If the Build.pm is not Panda::Tester the tests will be run as normal.
    Currently if a Build.pm file exists but does not supply a test method
    no tests are being run. This means many Nativecall reliant modules are
    not being tested in the smoketests etc.

--- lib/Panda/Tester.pm
+++ lib/Panda/Tester.pm
@@ -3,16 +3,19 @@ use Panda::Common;
 
 method test($where, :$prove-command = 'prove') {
     indir $where, {
+        my Bool $run-default = True;
         if "Build.pm".IO.f {
             @*INC.push('.');
             GLOBAL::<Build>:delete;
             require 'Build.pm';
             if ::('Build').isa(Panda::Tester) {
+                $run-default = False;
                 ::('Build').new.test($where, :$prove-command);
             }
             @*INC.pop;
         }
-        elsif 't'.IO ~~ :d {
+
+        if $run-default && 't'.IO ~~ :d {
             withp6lib {
                 my $c = "$prove-command -e $*EXECUTABLE -r t/";
                 shell $c or fail "Tests failed";
