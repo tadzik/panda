16df5f06bb53c182a84a78980fed47960a663946
Tadeusz Sośnierz <tadeusz.sosnierz@onet.pl>
2015-08-28 10:55:12 +0200

    Remove an obsolete topo-sort

--- lib/Panda/Common.pm
+++ lib/Panda/Common.pm
@@ -42,27 +42,6 @@ sub compsuffix is export { state $ = $*VM.precomp-ext }
 
 sub comptarget is export { state $ = $*VM.precomp-target }
 
-sub topo-sort(@modules, %dependencies) is export {
-    my @order;
-    my %color_of = flat @modules X=> 'not yet visited';
-    sub dfs-visit($module) {
-        %color_of{$module} = 'visited';
-        for %dependencies{$module}.list -> $used {
-            if (%color_of{$used} // '') eq 'not yet visited' {
-                dfs-visit($used);
-            }
-        }
-        push @order, $module;
-    }
-
-    for @modules -> $module {
-        if %color_of{$module} eq 'not yet visited' {
-            dfs-visit($module);
-        }
-    }
-    @order;
-}
-
 class X::Panda is Exception {
     has $.module is rw;
     has $.stage;
