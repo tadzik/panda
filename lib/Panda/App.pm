9c9522f4102677abd63cb2872676a258621435f2
Stefan Seifert <nine@detonation.org>
2015-12-01 09:09:20 +0100

    Gracefully deal with %*CUSTOM_LIB<home> not being available

--- lib/Panda/App.pm
+++ lib/Panda/App.pm
@@ -8,7 +8,7 @@ sub make-default-ecosystem(Str $prefix? is copy) is export {
     my $custom-prefix = ?$prefix;
     my $pandadir;
     $prefix = "$*CWD/$prefix" if defined($prefix) && !$*DISTRO.is-win && $prefix !~~ /^ '/' /;
-    for grep(*.defined, flat $prefix, %*CUSTOM_LIB<site home>.map(*.prefix.Str)) -> $target {
+    for grep(*.defined, flat $prefix, %*CUSTOM_LIB<site home>.grep(*.defined).map(*.prefix.Str)) -> $target {
         $prefix  = $target;
         $pandadir = "$target/panda".IO;
         try mkpath $pandadir unless $pandadir ~~ :d;
@@ -20,7 +20,7 @@ sub make-default-ecosystem(Str $prefix? is copy) is export {
 
     my @extra-statefiles;
     unless $custom-prefix or $prefix eq %*CUSTOM_LIB<site>.prefix {
-        for grep(*.defined, flat $prefix, %*CUSTOM_LIB<site home>.map(*.prefix.Str)) -> $target {
+        for grep(*.defined, flat $prefix, %*CUSTOM_LIB<site home>.grep(*.defined).map(*.prefix.Str)) -> $target {
             unless $prefix eq $target {
                 @extra-statefiles.push("$target/panda/state");
             }
