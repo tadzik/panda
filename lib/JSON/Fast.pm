6e273aeb32470283536a9c0ac4075f23600fde3b
Jonathan Stowe <jns@gellyfish.co.uk>
2016-04-25 15:33:35 +0100

    Squashed 'ext/JSON__Fast/' changes from 6d96209..db17c33
    db17c33 Fix for undefined value (#12)
    
    git-subtree-dir: ext/JSON__Fast
    git-subtree-split: db17c339cb9588341dbaaeec66aa37534690e382

--- lib/JSON/Fast.pm
+++ lib/JSON/Fast.pm
@@ -13,6 +13,7 @@ sub str-escape(str $text) {
 sub to-json($obj is copy, Bool :$pretty = True, Int :$level = 0, Int :$spacing = 2) is export {
     return $obj ?? 'true' !! 'false' if $obj ~~ Bool;
     return $obj.Str if $obj ~~ Int|Rat;
+    return 'null' if not $obj.defined;
     return "\"{str-escape($obj)}\"" if $obj ~~ Str;
 
     if $obj ~~ Seq {
